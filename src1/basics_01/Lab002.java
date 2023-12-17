7766e635c9c248baf495128556897a4534dfc1c6
Mamitha01 <mamithameenakshi2011@gmail.com>
2023-12-17 21:48:18 +0530

    My first commit on 17 dec

---
+++ src1/basics_01/Lab002.java
@@ -0,0 +1,18 @@
+package src1.basics_01;
+
+public class Lab002 {
+    public static void main(String[] args) {
+
+        byte age = 10; // Range of Byte -128 to 127
+        age = 127;
+        System.out.println(age + 1);
+        System.out.println(age);
+
+        // Local Variable - age
+
+
+        short _age = 128; //  range-32768 to 32678
+        System.out.println(_age);
+
+    }
+}
