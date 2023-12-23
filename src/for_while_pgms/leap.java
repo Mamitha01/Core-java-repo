e927cceb1d6272db59f1d373d57a914275ef40c6
Mamitha01 <mamithameenakshi2011@gmail.com>
2023-12-23 16:40:25 +0530

    My first commit on 23 dec

--- src/for_while_pgms/leap.java
+++ src/for_while_pgms/leap.java
@@ -8,7 +8,7 @@ public class leap {
         if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) {
             System.out.println(year + " is a leap year");
         }else {
-            System.out.println(year+" is not a leap year");
+            System.out.println(year +" is not a leap year");
         }
     }
 
